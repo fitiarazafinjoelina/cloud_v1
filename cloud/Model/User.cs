e97f4f2a42eb915b8cb5accb7a5dd04959c33acc
NathanalexK <miarynathan@hotmail.com>
2024-12-17 11:32:35 +0300

    Model User

--- cloud/Model/User.cs
+++ cloud/Model/User.cs
@@ -6,8 +6,15 @@ namespace cloud.Model;
 [Table("user")]
 public class User {
     [Key]
+    [Column("id_user")]
     public int IdUser { get; set; }
+
+    [Column("email")]
     public string Email { get; set; }
+
+    [Column("username")]
     public string Username { get; set; }
+
+    [Column("password")]
     public string Password { get; set; }
 }
