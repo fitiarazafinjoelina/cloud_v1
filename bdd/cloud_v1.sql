1e0fb8116db9119026e42146435e240a79157ca0
NathanalexK <miarynathan@hotmail.com>
2024-12-17 10:26:25 +0300

    base

---
+++ bdd/cloud_v1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "user"(
+    "id_user" SERIAL NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "id_role" INTEGER NOT NULL
+);
+ALTER TABLE
+    "user" ADD PRIMARY KEY("id_user");
+CREATE TABLE "role"(
+    "id_role" SERIAL NOT NULL,
+    "role" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "role" ADD PRIMARY KEY("id_role");
+ALTER TABLE
+    "user" ADD CONSTRAINT "user_id_role_foreign" FOREIGN KEY("id_role") REFERENCES "role"("id_role");
